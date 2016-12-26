e7211940d26a0ee1ce56389c5fe7ff7a10a9dbef
Danny O'Leary <dannyoleary1@gmail.com>
2016-12-26 14:46:17 +0000

    Added in CORS support to allow the app to connect

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
-
 var app = express(); 
+var cors = require('cors');
 
 require('./config/express').addMiddleware(app)
+app.use(cors());
 require('./routes')(app)
 
 app.listen(4000, function() {
