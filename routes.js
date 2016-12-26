30790886cb1e6d072537be8b78320143094d1e1c
Danny O'Leary <dannyoleary1@gmail.com>
2016-12-26 13:47:44 +0000

    Added in another API for teams

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 
   app.use('/api/players', require('./api/players/index'));
-  //app.use('/api/statistics', require('./api/product'));
+  app.use('/api/teams', require('./api/teams/index'));
   
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|app|assets)/*')
