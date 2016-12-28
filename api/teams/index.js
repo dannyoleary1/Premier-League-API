e275364ab15b49babccffb1a9600169c1e6ee066
Danny O'Leary <dannyoleary1@gmail.com>
2016-12-28 15:39:51 +0000

    Added functionality for using put

--- api/teams/index.js
+++ api/teams/index.js
@@ -7,5 +7,6 @@ router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.delete('/:id', controller.destroy);
+router.put('/:id', controller.update);
 
 module.exports = router;
